81fec1a81da8d43dcde49a519908bdc0799967e7
Oussama Saibi <saibioussama@outlook.fr>
2017-11-23 09:40:28 +0100

    correctMessages

--- app/Http/Controllers/messagesController.php
+++ app/Http/Controllers/messagesController.php
@@ -9,7 +9,7 @@ use App\Message;
 class messagesController extends Controller
 {
     public function store(Request $request){
-        $request['email']=$request->ip();
+        $request['ip_address']=$request->ip();
         Message::create($request->all());
         return View('profile.index');
     }
--- database/migrations/2017_11_23_075137_create_messages_table.php
+++ database/migrations/2017_11_23_075137_create_messages_table.php
@@ -6,26 +6,17 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateMessagesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
             $table->string('email')->nullable();
+            $table->string('ip_address')->nullable();
             $table->string('content')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('messages');
